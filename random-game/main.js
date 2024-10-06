0afff6663ecb171a4d4f2a61c9c8ad1ca2f59a93
Viktoriia Svichkar <vitasvichkar@gmail.com>
2024-10-06 07:27:13 +0300

    feat: create a method for drawing tiles in a random location(addTile).  Add a function to get a random number(getRandom).

--- random-game/main.js
+++ random-game/main.js
@@ -6,6 +6,7 @@ const c = {
   widthCanvas: 400,
   heightCanvas: 400,
   numberTile: 4,
+  arrayValues: [],
   init() {
     this.canvas = document.getElementById('canvas');
     this.canvas.width = this.widthCanvas;
@@ -14,29 +15,53 @@ const c = {
     this.ctx.fillStyle = '#fff';
     this.ctx.fillRect(0, 0, this.widthCanvas, this.heightCanvas);
 
+    this.createMatrix();
     this.drawTile();
   },
 
   drawTile() {
+    this.addTile();
     const widthTile = this.widthCanvas / this.numberTile;
     const heightTile = widthTile;
+    const arrNew = [];
+
     let x = 0;
     let y = 0;
-    for (let i = 0; i < this.numberTile; i += 1) {
-      y = i * widthTile;
-      for (let j = 0; j < this.numberTile; j += 1) {
-        x = j * widthTile;
-        this.ctx.strokeRect(x, y, widthTile, heightTile);
-        this.ctx.font = '40px Arial';
-        this.ctx.fillStyle = 'red';
-        this.ctx.textAlign = 'center';
-        this.ctx.textBaseline = 'middle';
-        this.ctx.fillText('2', x + widthTile / 2, y + heightTile / 2);
-      }
-    }
+    this.arrayValues.forEach((row, indY) => {
+      y = indY * widthTile;
+      row.forEach((val, indX) => {
+        x = indX * widthTile;
+        // this.ctx.strokeRect(x, y, widthTile, heightTile);
+        if (val !== 0) {
+          this.ctx.strokeRect(x, y, widthTile, heightTile);
+          this.ctx.font = '40px Arial';
+          this.ctx.fillStyle = 'red';
+          this.ctx.textAlign = 'center';
+          this.ctx.textBaseline = 'middle';
+          this.ctx.fillText(`${val}`, x + widthTile / 2, y + heightTile / 2);
+        }
+      });
+    });
+  },
+
+  addTile() {
+    const row = this.getRandom();
+    const col = this.getRandom();
+    const r = Math.random() >= 0.5 ? 2 : 4;
+    this.arrayValues[row][col] = r;
+  },
+
+  getRandom() {
+    return Math.floor(Math.random() * 4);
+  },
+
+  move(e) {
+    console.log(e);
   },
 };
 
 document.addEventListener('DOMContentLoaded', () => {
   c.init();
 });
+
+document.addEventListener('keydown', c.move.bind(c));
