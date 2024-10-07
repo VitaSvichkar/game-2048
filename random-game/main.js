cdacff7f130b9816b960fcd8b80f7fe51496a9dc
Viktoriia Svichkar <vitasvichkar@gmail.com>
2024-10-07 19:52:56 +0300

    fix: add NaN check (changeMatrix) and prevent infinite loop in function(addTile). Add comments

--- random-game/main.js
+++ random-game/main.js
@@ -7,31 +7,34 @@ const c = {
   heightCanvas: 400,
   numberTile: 4,
   arrayValues: [],
+
+  // START GAME
+
   init() {
     this.canvas = document.getElementById('canvas');
     this.canvas.width = this.widthCanvas;
     this.canvas.height = this.heightCanvas;
     this.ctx = this.canvas.getContext('2d');
-    this.ctx.fillStyle = '#fff';
-    this.ctx.fillRect(0, 0, this.widthCanvas, this.heightCanvas);
 
     this.createMatrix();
-    this.drawTile();
+    this.addTile();
   },
 
+  // DRAW TILE
+
   drawTile() {
-    this.addTile();
+    this.ctx.clearRect(0, 0, this.widthCanvas, this.heightCanvas);
+    this.ctx.fillStyle = '#fff';
+    this.ctx.fillRect(0, 0, this.widthCanvas, this.heightCanvas);
     const widthTile = this.widthCanvas / this.numberTile;
     const heightTile = widthTile;
-    const arrNew = [];
-
     let x = 0;
     let y = 0;
+
     this.arrayValues.forEach((row, indY) => {
       y = indY * widthTile;
       row.forEach((val, indX) => {
         x = indX * widthTile;
-        // this.ctx.strokeRect(x, y, widthTile, heightTile);
         if (val !== 0) {
           this.ctx.strokeRect(x, y, widthTile, heightTile);
           this.ctx.font = '40px Arial';
@@ -44,16 +47,32 @@ const c = {
     });
   },
 
+  // ADD TILE
+
   addTile() {
-    const row = this.getRandom();
-    const col = this.getRandom();
+    let row = this.getRandom();
+    let col = this.getRandom();
     const r = Math.random() >= 0.5 ? 2 : 4;
+    if (this.arrayValues.flat().some((val) => val === 0)) {
+      while (this.arrayValues[row][col] !== 0) {
+        row = this.getRandom();
+        col = this.getRandom();
+      }
+    } else {
+      window.alert('loser');
+    }
+
     this.arrayValues[row][col] = r;
+    this.drawTile();
   },
 
+  // GET RANDOM NUMBER
+
   getRandom() {
-    return Math.floor(Math.random() * 4);
+    return Math.floor(Math.random() * this.numberTile);
   },
+
+  // CREATE MATRIX
 
   createMatrix() {
     for (let i = 0; i < this.numberTile; i += 1) {
@@ -64,26 +83,32 @@ const c = {
     }
   },
 
+  // CHANGE MATRIX (filter array)
+
   changeMatrix(e) {
     if (e.key === 'ArrowLeft') {
-      const filteredArray = this.arrayValues.map((row) => {
-        return row.filter((el) => el);
+      let filteredArray = this.arrayValues.map((row) => {
+        return row.filter((el) => el !== 0);
       });
-
+      console.log(filteredArray);
       filteredArray.forEach((row) => {
-        for (let i = 0; i < row.length; i += 1) {
+        for (let i = 0; i < this.numberTile; i += 1) {
+          if (isNaN(row[i])) {
+            row[i] = 0;
+          }
           if (row[i] === row[i + 1]) {
             row[i] += row[i + 1];
-            row[i + 1] = null;
+            row[i + 1] = 0;
             i += 1;
           }
         }
       });
+      console.log(filteredArray);
 
-      filteredArray.forEach((row) => {
-        row.filter((el) => el !== null);
+      filteredArray.forEach((row, index) => {
+        filteredArray[index] = row.filter((el) => el !== 0);
       });
-
+      // console.log(filteredArray);
       filteredArray.forEach((row) => {
         for (let i = 0; i < this.numberTile; i += 1) {
           if (!row[i]) {
@@ -91,18 +116,24 @@ const c = {
           }
         }
       });
-      this.drawTile();
-      console.log(filteredArray);
+      this.arrayValues = [...filteredArray];
+      this.addTile();
     }
   },
+
+  // KEY
 
   move(e) {
     this.changeMatrix(e);
   },
 };
 
+// LOAD
+
 document.addEventListener('DOMContentLoaded', () => {
   c.init();
 });
+
+// CLICK
 
 document.addEventListener('keydown', c.move.bind(c));
